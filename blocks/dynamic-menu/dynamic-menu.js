3e9e14399a8da6b38c10603a17e7956c11755bc7
Helmut Minst <helmut.minst@valtech.com>
2024-05-02 12:14:26 +0200

    100

--- blocks/dynamic-menu/dynamic-menu.js
+++ blocks/dynamic-menu/dynamic-menu.js
@@ -1,11 +1,5 @@
 export default function decorate(block) {
-    let metaDescription = document.querySelector('meta[name="description"]')
-    
-    if (!metaDescription) {
-        metaDescription = document.createElement('meta')
-    }
-    metaDescription.setAttribute("content", "this is our main menu");
-    
+
     const mabMenu = document.createElement('mab-menu');
     mabMenu.setAttributeNS(null, 'id',"Trading Entity.12345");
     mabMenu.setAttributeNS(null, "api-url", 'https://api-development.mbplc.io/mabapi-menu-v2-poc/api/v1/menus/dynamic')
