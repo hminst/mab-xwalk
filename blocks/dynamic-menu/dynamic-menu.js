5bbcfc08d7ef80a17987a1a6272c668ba4c2b6fd
Helmut Minst <helmut.minst@valtech.com>
2024-05-02 14:06:52 +0200

    minimise layoutshift

--- blocks/dynamic-menu/dynamic-menu.js
+++ blocks/dynamic-menu/dynamic-menu.js
@@ -1,11 +1,13 @@
 export default function decorate(block) {
     const mabMenu = document.createElement('mab-menu');
+    const api = block.querySelectorAll('div > p > a')[0].href
+    
     mabMenu.setAttributeNS(null, 'id',"Trading Entity.12345");
-    mabMenu.setAttributeNS(null, "api-url", 'https://api-development.mbplc.io/mabapi-menu-v2-poc/api/v1/menus/dynamic')
+    mabMenu.setAttributeNS(null, "api-url", api)
     mabMenu.setAttributeNS(null, 'brand-id',"Example brand");
     mabMenu.setAttributeNS(null, 'id-type',"brand");
     mabMenu.setAttributeNS(null, 'menu-description-override',"Menu Description Override");
-    mabMenu.setAttributeNS(null, 'menu-item-highlight-text',"Highlight Text");
+    mabMenu.setAttributeNS(null, 'menu-item-highlight-text',"Today's special");
     mabMenu.setAttributeNS(null, 'menu-name-override',"Menu Name Override");
     mabMenu.setAttributeNS(null, 'page-path',"harvester.co.uk");
     mabMenu.setAttributeNS(null, 'smart-chef-url',"https://www.smartchef.co.uk/brands/VintageInns");    
