e7fe0f4d1965717d80242c3761de84ac31abed46
Helmut Minst <helmut.minst@valtech.com>
2024-05-02 14:55:55 +0200

    do not load early anymore...

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -6,12 +6,11 @@ sampleRUM('cwv');
 
 // add more delayed functionality here
 
-/*
+
 let coreComp = document.createElement("script");
-    coreComp.setAttribute("src", "/scripts/mab/container.js");
-    document.body.appendChild(coreComp);
-    
-    let scriptEle = document.createElement("script");
-    scriptEle.setAttribute("src", "/scripts/mab/clientlib-base.js");
-    document.body.appendChild(scriptEle);
-*/
+coreComp.setAttribute("src", "/scripts/mab/container.js");
+document.body.appendChild(coreComp);
+
+let scriptEle = document.createElement("script");
+scriptEle.setAttribute("src", "/scripts/mab/clientlib-base.js");
+document.body.appendChild(scriptEle);
