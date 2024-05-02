c20502dcdd2f31915e25770495ae24457dfe3bfb
Helmut Minst <helmut.minst@valtech.com>
2024-05-02 14:17:56 +0200

    config from aem and preload logo

--- blocks/dynamic-menu/dynamic-menu.js
+++ blocks/dynamic-menu/dynamic-menu.js
@@ -1,12 +1,12 @@
 export default function decorate(block) {
     const mabMenu = document.createElement('mab-menu');
     const api = block.querySelectorAll('div > p > a')[0].href
-    
+    const pTags = block.querySelectorAll('div > p');
     mabMenu.setAttributeNS(null, 'id',"Trading Entity.12345");
     mabMenu.setAttributeNS(null, "api-url", api)
     mabMenu.setAttributeNS(null, 'brand-id',"Example brand");
     mabMenu.setAttributeNS(null, 'id-type',"brand");
-    mabMenu.setAttributeNS(null, 'menu-description-override',"Menu Description Override");
+    mabMenu.setAttributeNS(null, 'menu-description-override',pTags[3].innerHTML);
     mabMenu.setAttributeNS(null, 'menu-item-highlight-text',"Today's special");
     mabMenu.setAttributeNS(null, 'menu-name-override',"Menu Name Override");
     mabMenu.setAttributeNS(null, 'page-path',"harvester.co.uk");
